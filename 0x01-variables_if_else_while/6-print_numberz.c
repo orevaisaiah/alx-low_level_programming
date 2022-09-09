e1fc384301a7d2f7d8a8426a6d1491e6fb76425f
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-09 09:50:13 -0700

    Commit for 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * You are not allowed to use any variable of type char
+ * You can only use the putchar function
+ * (every other function (printf,
+ * puts, etc…) is forbidden)
+ * You can only use putchar twice in your code
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0: n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	putchar('/n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -4,3 +4,5 @@
 3-print_alphabets.c: This program prints the alphabet in lowercase, and then in uppercase, followed by a new line.
 4-print_alphabt.c: This program prints the alphabet in lowercase, followed by a new line.
 5-print_numbers.c: This program prints all single digit numbers of base 10 starting from 0, followed by a new line.
+6-print_numberz.c: This program  prints all single digit numbers of base 10 starting from 0, followed by a new line.
+
