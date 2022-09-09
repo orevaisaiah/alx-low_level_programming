d1be384812672dbaea7cf1a9a04100283913712f
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-09 08:48:21 -0700

    Commit for 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,14 @@ int main(void)
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
-        {
-                putchar(n);
-        }
+	{
+		putchar(n);
+	}
 	for (n = 'A'; n <= 'Z'; n++)
 	{
 		putchar(n);
 	}
 	putchar('\n');
-
 	return (0);
 
 }
