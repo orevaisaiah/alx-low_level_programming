c0e073d397e8be9048abf428c2b5557a9b863eea
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-09 06:52:01 -0700

    Commit for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		prinf("%d is positive/n", n)
+		prinf("%d is positive/n", n);
 	else if (n == 0)
-		printf("%d is zero", n)
+		printf("%d is zero", n);
 	else (n < 0)
-		printf("%d is negative", n)
+		printf("%d is negative", n);
 	return (0);
 }
