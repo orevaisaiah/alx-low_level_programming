00bfc85054064af9a7f124f947e3211291592171
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-21 06:01:04 -0700

    Commit for 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: Pointer to first string to compare
+ * @s2: Pointer to second string to compare
+ * Return: less than -1 if s1 is less than s2, 0 if they're equal,
+ * more than 1 if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (s1)
+	{
+
+		if (s1 != s2) 
+		{
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (s1 - s2);
+}
