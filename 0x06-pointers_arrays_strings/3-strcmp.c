12b246059f1dbd10850f13086a33e03165dd7cfd
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-22 01:48:05 -0700

    Commit for 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (s1)
+	if (s1 < s2)
 	{
-
-		if (s1 != s2) 
-		{
-			break;
-		}
-		s1++;
-		s2++;
+		return (*s1 - *s2);
 	}
-	return (s1 - s2);
+	else if (s1 > s2)
+	{
+		return (*s1 - *s2;
+	}
+	else
+		return (0);
 }
