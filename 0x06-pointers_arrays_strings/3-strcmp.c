da731f7f5a439bdf41534377ea70acdb6faae657
orevaisaiah <inabororevaoghene@gmail.com>
2022-09-22 01:50:26 -0700

    Commit for 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,20 @@
  * _strcmp - compares two strings
  * @s1: Pointer to first string to compare
  * @s2: Pointer to second string to compare
- * Return: less than -1 if s1 is less than s2, 0 if they're equal,
- * more than 1 if s1 is greater than s2
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
+	while (*s1 == *s2)
 	{
-		return (*s1 - *s2);
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	else if (s1 > s2)
-	{
-		return (*s1 - *s2;
-	}
-	else
-		return (0);
+	return (*s1 - *s2);
 }
