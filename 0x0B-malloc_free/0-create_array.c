43a2c6e24bed375fdad7999923a425474b44927c
orevaisaiah <inabororevaoghene@gmail.com>
2022-10-03 11:22:00 -0700

    Commit for 0x0B-malloc_free Mandatory tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - A function that creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
+	{
+		p = (char *)malloc(size * sizeof(char));
+		if (p != NULL)
+		{
+			for (i = 0; i < size; i++)
+				p[i] = c;
+		}
+		p[i + 1] = ''\0';
+	}
+	return (p);
+}
